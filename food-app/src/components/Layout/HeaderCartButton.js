66d6b27d30d8fd81bf450bd6fa59cd3a338a32aa
youss95 <usekim7555@gmail.com>
2022-04-21 21:29:32 +0900

    카트 추가시 버튼 범프 애니메이션

--- food-app/src/components/Layout/HeaderCartButton.js
+++ food-app/src/components/Layout/HeaderCartButton.js
@@ -1,15 +1,33 @@
-import { useContext } from "react";
+import { useContext, useState, useEffect } from "react";
 import CartContext from "../../store/cart-context";
 import CartIcon from "../Cart/CartIcon";
 import "./HeaderCartButton.css";
 const HeaderCartButton = ({ showCartHandler }) => {
   const cartCtx = useContext(CartContext);
+  const { items } = cartCtx;
+  const [btnHigh, setBtnHigh] = useState(false);
   const cartItemsNum = cartCtx.items.reduce((cur, item) => {
     return cur + item.amount;
   }, 0);
-  console.log(cartCtx);
+  useEffect(() => {
+    if (cartCtx.items.length === 0) {
+      return;
+    }
+    setBtnHigh(true);
+    const timer = setTimeout(() => {
+      setBtnHigh(false);
+    }, 300);
+    //cleanup
+    return () => {
+      console.log("off");
+      clearTimeout(timer);
+    };
+  }, [items]);
   return (
-    <button className="button" onClick={showCartHandler}>
+    <button
+      className={["button", `${btnHigh ? "bump" : ""}`].join(" ")}
+      onClick={showCartHandler}
+    >
       <span className="icon">
         <CartIcon />
       </span>
