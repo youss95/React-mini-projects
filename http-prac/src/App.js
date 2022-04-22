ba46538da1eca989aff08b514e2bfb4edb6e5737
youss95 <usekim7555@gmail.com>
2022-04-22 22:00:37 +0900

    using api prac

---
+++ http-prac/src/App.js
@@ -0,0 +1,38 @@
+import React, { useState } from "react";
+
+import MoviesList from "./components/MoviesList";
+import "./App.css";
+
+function App() {
+  const [movies, setMovies] = useState([]);
+  function fetchMovie() {
+    fetch("https://swapi.dev/api/films")
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        const transformedMovies = data.results.map((movieData) => {
+          return {
+            id: movieData.episode_id,
+            title: movieData.title,
+            openingText: movieData.opening_crawl,
+            releaseDate: movieData.release_date,
+          };
+        });
+        setMovies(transformedMovies);
+      });
+  }
+
+  return (
+    <React.Fragment>
+      <section>
+        <button onClick={fetchMovie}>Fetch Movies</button>
+      </section>
+      <section>
+        <MoviesList movies={movies} />
+      </section>
+    </React.Fragment>
+  );
+}
+
+export default App;
